e4dd5cbdf98aa4b5f995b4cd17a43b0d20d42585
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-05-29 19:24:19 +0530

    Initial commit: Blender

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      packages.${system}.blender = pkgs.blender.override {
+        cudaSupport = true;
+      };
+    };
+}
